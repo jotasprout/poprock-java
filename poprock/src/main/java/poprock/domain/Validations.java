65c14d0bdc61a4b51845d9c3b6d2560c6f5d8072
Jay Sprout <jotasprout@gmail.com>
2023-10-20 14:54:42 +0200

    finalizing add artist from api result in browser

---
+++ poprock/src/main/java/poprock/domain/Validations.java
@@ -0,0 +1,9 @@
+package learn.field_agent.domain;
+
+public class Validations {
+
+    public static boolean isNullOrBlank(String value) {
+        return value == null || value.isBlank();
+    }
+
+}
